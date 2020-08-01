860a7f0f0cd1ef0113f94e5e2008a96c1e662ab4
Jonas Degnan <jonas.degnan@gmail.com>
2020-07-31 15:49:11 -1000

    changed unique_numbers to utilize set operations

--- Project1/P0/Task1.py
+++ Project1/P0/Task1.py
@@ -19,10 +19,9 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 records = texts + calls #merge lists to get total recordset #O(1)
-unique_numbers = [] #initialize unique numbers list #O(1)
+unique_numbers = set()  #initialize unique numbers list #O(1)
 for item in records: #O(n)
-    for numbers in item[:2]: #O(1) -> item[:2]
-        if numbers not in unique_numbers:
-            unique_numbers.append(numbers) #O(1)
+    unique_numbers.add(item[0])
+    unique_numbers.add(item[1])
 
 print("There are",len(unique_numbers),"different telephone numbers in the records.") #O(1)

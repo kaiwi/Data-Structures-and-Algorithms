327d1622449e1d08635d6629f8a002a73ec0de94
Jonas Degnan <jonas.degnan@gmail.com>
2020-08-05 12:06:58 -1000

    Changed to use set operations.

--- Project1/P0/Task4.py
+++ Project1/P0/Task4.py
@@ -25,20 +25,21 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 #Make list of possible telemarkets
-telemarketer_possibles = [] #O(1); initialize telemarketer list
+telemarketer_possibles = set() #O(1); initialize telemarketer set
 
 #Must make outgoing calls
-outgoing_calls = [caller for caller, recipient, datetime, duration in calls] #O(1); slice outgoing calls
-incoming_calls = [recipient for caller,recipient, datetime, duration in calls] #O(1); slice incoming calls
-outgoing_texts = [texter for texter, textee, datetime in texts] #O(1); slice outgoing texts
-incoming_texts = [textee for texter, textee, datetime in texts] #O(1); slice incoming texts
+outgoing_calls = {caller for caller, recipient, datetime, duration in calls}
+incoming_calls = {recipient for caller,recipient, datetime, duration in calls}
+outgoing_texts = {texter for texter, textee, datetime in texts}
+incoming_texts = {textee for texter, textee, datetime in texts}
 
 #No receiving texts, No receiving calls, No sending texts
 for caller in outgoing_calls: #O(n)
-    if (caller not in incoming_calls) and (caller not in outgoing_texts) and (caller not in incoming_texts):
-        telemarketer_possibles.append(caller)
+    if (caller not in incoming_calls) and (caller not in outgoing_texts) and (caller not in incoming_texts): #O(3)
+        telemarketer_possibles.add(caller) #O(1)
 
 #print result
 print("These numbers could be telemarketers: ") #O(1)
 for item in sorted(list(set(telemarketer_possibles))): #O(n log n) + O(n)
    print(item)
+

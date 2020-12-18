6da6b6e474cf34740a1760053f8099afd308cdf2
Jonas Degnan <jonas.degnan@gmail.com>
2020-12-18 11:07:28 -1000

    Completed union() method.

--- Project2/problem_6.py
+++ Project2/problem_6.py
@@ -3,7 +3,12 @@
 #        Union: set of elements which are in A, in B, or in both A and B.
 # Intersection: set of all objects that are members of both the sets A and B.
 
-class Node:
+# Take in two linked lists and return a linked list that is composed of either the union or intersection, respectively.
+# Once you have completed the problem you will create your own test cases and perform your own run time analysis on the
+# code.
+from time import sleep
+
+class Node(object):
     def __init__(self, value):
         self.value = value
         self.next = None
@@ -12,7 +17,7 @@ class Node:
         return str(self.value)
 
 
-class LinkedList:
+class LinkedList(object):
     def __init__(self):
         self.head = None
 
@@ -23,7 +28,6 @@ class LinkedList:
             out_string += str(cur_head.value) + " -> "
             cur_head = cur_head.next
         return out_string
-
 
     def append(self, value):
 
@@ -46,12 +50,26 @@ class LinkedList:
 
         return size
 
+
 def union(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    """
+    Returns a LinkedList containing all elements from both arguments.
+    :param llist_1: A LinkedList.
+    :param llist_2: A LinkedList.
+    :return:
+    """
+    head_list = [llist_1.head, llist_2.head]
+    llist_union = LinkedList()
+    for head in head_list:  # iterate through LinkedList heads
+        node = head
+        while node is not None:  # iterate through each LinkedList
+            llist_union.append(node.value)  # add a new node to union LinkedList
+            node = node.next
+
+    return llist_union
+
 
 def intersection(llist_1, llist_2):
-    # Your Solution Here
     pass
 
 
@@ -61,8 +79,8 @@ if __name__ == "__main__":
     linked_list_1 = LinkedList()
     linked_list_2 = LinkedList()
 
-    element_1 = [3,2,4,35,6,65,6,4,3,21]
-    element_2 = [6,32,4,9,6,1,11,21,1]
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
 
     for i in element_1:
         linked_list_1.append(i)
@@ -70,16 +88,16 @@ if __name__ == "__main__":
     for i in element_2:
         linked_list_2.append(i)
 
-    print (union(linked_list_1,linked_list_2))
-    print (intersection(linked_list_1,linked_list_2))
+    print(union(linked_list_1, linked_list_2))
+    # print(intersection(linked_list_1, linked_list_2))
 
     # Test case 2
 
     linked_list_3 = LinkedList()
     linked_list_4 = LinkedList()
 
-    element_1 = [3,2,4,35,6,65,6,4,3,23]
-    element_2 = [1,7,8,9,11,21,1]
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+    element_2 = [1, 7, 8, 9, 11, 21, 1]
 
     for i in element_1:
         linked_list_3.append(i)
@@ -87,5 +105,5 @@ if __name__ == "__main__":
     for i in element_2:
         linked_list_4.append(i)
 
-    print (union(linked_list_3,linked_list_4))
-    print (intersection(linked_list_3,linked_list_4))
+    print(union(linked_list_3, linked_list_4))
+    # print(intersection(linked_list_3, linked_list_4))
